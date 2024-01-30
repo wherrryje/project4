3f936ae3a61b9dd3596e6200fb13baac802fd27d
wherrryje <146250438+wherrryje@users.noreply.github.com>
2024-01-30 09:45:45 -1000

    Add files via upload

--- bin_except.py
+++ bin_except.py
@@ -1,11 +1,11 @@
 class TargetNotFound(Exception):
     pass
 
-def linear_search(a_list, target):
+def bin_except(a_list, target):
     """
     Searches a_list for an occurrence of target
     If found, returns the index of its position in the list
-    If not found, returns -1, indicating the target value isn't in the list
+    If not found, raises exception of TargetNotFound
     """
     # enumerate lets you iterate through both the elements of the list and their indices
     for index, el in enumerate(a_list):
